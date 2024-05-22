94d31f494a5e0276734aed0597e6966c2297989f
Sameh Makkie <smhmkkh@hotmail.co.uk>
2024-05-23 01:29:11 +0300

    Hide the circular mouse follower if the device doesn't have a pointer

--- components/MouseFollower/MouseFollower.tsx
+++ components/MouseFollower/MouseFollower.tsx
@@ -5,10 +5,29 @@ import { motion, useMotionValue, animate } from "framer-motion";
 const MouseFollower: React.FC = () => {
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
   const [isHoveringTarget, setIsHoveringTarget] = useState(false);
+  const [showMouseFollower, setShowMouseFollower] = useState(false);
   const x = useMotionValue(0);
   const y = useMotionValue(0);
 
   useEffect(() => {
+    // Check if the device has a fine pointer (e.g., a mouse)
+    const mediaQuery = window.matchMedia('(pointer: fine)');
+    setShowMouseFollower(mediaQuery.matches);
+
+    // Add event listener to update the state if the media query changes
+    const handleMediaChange = (event: MediaQueryListEvent) => {
+      setShowMouseFollower(event.matches);
+    };
+    mediaQuery.addEventListener('change', handleMediaChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!showMouseFollower) return;
+
     const handleMouseMove = (event: MouseEvent) => {
       setMousePosition({ x: event.clientX, y: event.clientY });
 
@@ -25,9 +44,11 @@ const MouseFollower: React.FC = () => {
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
     };
-  }, []);
+  }, [showMouseFollower]);
 
   useEffect(() => {
+    if (!showMouseFollower) return;
+
     const controlsX = animate(x, mousePosition.x, { type: "spring", stiffness: 300, damping: 30 });
     const controlsY = animate(y, mousePosition.y, { type: "spring", stiffness: 300, damping: 30 });
 
@@ -35,7 +56,11 @@ const MouseFollower: React.FC = () => {
       controlsX.stop();
       controlsY.stop();
     };
-  }, [mousePosition]);
+  }, [mousePosition, showMouseFollower]);
+
+  if (!showMouseFollower) {
+    return null;
+  }
 
   return (
     <motion.div
